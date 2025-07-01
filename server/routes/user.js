d8265aa91abc9ac5115fdf97f0086d33f0d06d1e
Suryam <suryamkandapu205@gmail.com>
2025-07-01 23:20:20 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -76,9 +76,9 @@ router.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'Lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
+      secure: true,           
+      sameSite: 'None',       
+  });
 
 
     res.status(200).json({
