12c4d2fe64a97c5adf17bcdbdbdee1ba8ae2fb78
Suryam <suryamkandapu205@gmail.com>
2025-07-01 23:08:49 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import { Link } from "react-router-dom"; // for routing to /article/:slug
+import { Link } from "react-router-dom"; 
 import Navbar from "../components/Navbar";
 import GetPost from "./GetPost";
 import GenerateBlogs from "../components/GenerateBlogs";
